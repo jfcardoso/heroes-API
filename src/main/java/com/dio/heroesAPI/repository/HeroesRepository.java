0c70bb3dfe0f0d225501b5e51e11660ecd8e78ad
jfcardoso <maestro.jfc@gmail.com>
2021-12-07 16:16:15 -0300

    Initial commit

---
+++ src/main/java/com/dio/heroesAPI/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.dio.heroesAPI.repository;
+
+import com.dio.heroesAPI.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
